2be77e2512fbaeabcff107bc647a742d32566f83
Lymperis E <geo.elymperis@gmail.com>
2022-12-25 22:03:08 +0200

    .

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -10,31 +10,31 @@ function Social(){
         <div id="social">
              
     
-            <div class="social-btn">
+            <div className="social-btn">
                 <a href="https://www.linkedin.com/in/efstathios-lymperis-975702188/" target="_blank" role="button">
-                    <img class="social-btn-img rounded-circle " src={LinkedInImg} alt="Connect on LinkedIn!" width="50"
+                    <img className="social-btn-img rounded-circle " src={LinkedInImg} alt="Connect on LinkedIn!" width="50"
                         height="50"></img>
                 </a>
 
             </div>
 
-            <div class="social-btn">
+            <div className="social-btn">
                 <a href="https://github.com/lymperis-e" target="_blank" role="button">
-                    <img class="social-btn-img rounded-circle " src={GitHubImg} alt="Let's connect on GitHub!" width="50"
+                    <img className="social-btn-img rounded-circle " src={GitHubImg} alt="Let's connect on GitHub!" width="50"
                         height="50" ></img>
                 </a>
             </div>
 
-            <div class="social-btn">
+            <div className="social-btn">
                 <a href="https://www.researchgate.net/profile/Efstathios-Lymperis" target="_blank" role="button">
-                    <img class="social-btn-img rounded-circle " src={RGImg} alt="Let's connect on ResearchGate!"
+                    <img className="social-btn-img rounded-circle " src={RGImg} alt="Let's connect on ResearchGate!"
                         width="50" height="50"></img>
                 </a>
             </div>
 
-            <div class="social-btn">
+            <div className="social-btn">
                 <a href="mailto:geo.elymperis@gmail.com" role="button">
-                    <img class="social-btn-img rounded-circle " src={EmailImg} alt="Send an email!" width="50" height="50"></img>
+                    <img className="social-btn-img rounded-circle " src={EmailImg} alt="Send an email!" width="50" height="50"></img>
                 </a>
             </div>
 
